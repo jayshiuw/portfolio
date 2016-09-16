1fbe23285cea706a775d379913ef9099fd1f8c69
jayshiuw <jayshi.uw@gmail.com>
2016-09-16 10:47:53 -0400

    Update sayHello.js

--- sayHello.js
+++ sayHello.js
@@ -1,4 +1,10 @@
+'use strict';
+
 var userName = prompt('what's your name? ');
+
 alert('hello, '+ userName +'welcome to my site ');
-intro.click(function (){
- alert('you will be redirected to another website')});
+
+intro.click(
+  function (){
+   alert('you will be redirected to another website'); }
+ );
