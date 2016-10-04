f83d5f23022f195a25c5fa7a221dfcddb756a909
jayshiuw <jayshi.uw@gmail.com>
2016-10-04 14:54:52 -0400

    Update sayHello.js

--- sayHello.js
+++ sayHello.js
@@ -4,5 +4,5 @@ var userName = prompt("what's your name?") ;
 
 alert( "hello, "+ userName +"welcome to my site" ) ;
 
-$('.jobexp').on('click', 
+$('#jobexp').on('click', 
                  function() { var opinion = prompt("what do you think about this section? ")});
