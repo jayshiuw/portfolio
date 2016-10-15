8f17f8fb4bfdf04b505b199384439ab55ccfccbd
jayshiuw <jayshi.uw@gmail.com>
2016-10-15 18:47:51 -0400

    Delete sayHello.js

--- sayHello.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var userName = prompt("what's your name?") ;
-
-alert( "hello, "+ userName +"welcome to my site" ) ;
-
-$('#jobexp').on('click', 
-                 function() { var opinion = prompt("what do you think about this section? ")});
